b9016e11a76bc867e0d89b61fb7af2a092a134c2
hextion <100ishundred@gmail.com>
2025-04-10 14:09:01 +0300

    chore: test

--- tools/gh-actions/commit-and-push/main.js
+++ tools/gh-actions/commit-and-push/main.js
@@ -1,18 +1,18 @@
 module.exports = async ({ context, core, exec, inputs }) => {
-    const fs = require('node:fs/promises');
+    // const fs = require('node:fs/promises');
 
-    const githubToken = inputs['github-token'];
+    // const githubToken = inputs['github-token'];
 
-    if (githubToken === '') {
-        core.setFailed('Please add GITHUB_TOKEN to the action input');
+    // if (githubToken === '') {
+    //     core.setFailed('Please add GITHUB_TOKEN to the action input');
 
-        return;
-    }
+    //     return;
+    // }
 
-    await fs.writeFile(
-        `${process.env.HOME}/.netrc`,
-        `machine github.com\nlogin github-actions[bot]\npassword ${githubToken}`,
-    );
+    // await fs.writeFile(
+    //     `${process.env.HOME}/.netrc`,
+    //     `machine github.com\nlogin github-actions[bot]\npassword ${githubToken}`,
+    // );
 
     await exec.exec('git', ['config', 'user.name', `"github-actions[bot]"`]);
 
