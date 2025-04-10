4df67b46102d40c2a67840a395883381eacb737b
hextion <100ishundred@gmail.com>
2025-04-10 11:28:39 +0300

    chore: up playwright version

--- packages/screenshot-utils/setupScreenshotTesting.ts
+++ packages/screenshot-utils/setupScreenshotTesting.ts
@@ -1,4 +1,4 @@
-import { Browser, BrowserContext, chromium, Page } from 'playwright';
+import { Browser, BrowserContext, chromium, Page } from '@playwright/test';
 
 import { defaultViewport, matchHtml, MatchHtmlParams } from './helpers';
 
